d068e039224acdc22dc16a732a781c42756758e1
Ameer Hamza <76493021+ameerhamza006@users.noreply.github.com>
2021-04-16 15:40:27 +0500

    role table

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -2,6 +2,14 @@
 
 @section('content')
 <div class="container">
+
+@php
+
+$see = App\Models\Role::all();
+
+
+ @endphp
+
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
@@ -10,6 +18,31 @@
                 <div class="card-body">
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
+
+
+                        <div class="form-group row">
+                            <label for="role" class="col-md-4 col-form-label text-md-right">{{ __('Role') }}</label>
+
+                            <div class="col-md-6">
+                            <select   class="form-control @error('role') is-invalid @enderror" name="role" value="{{ old('role') }}" required>
+                            <option selected disabled>--Select Role--</option>
+                            @foreach($see as $role)
+                            <option value="{{$role->id}}" >{{$role->name}}</option>
+                           @endforeach
+                            
+                            </select>
+                                
+
+                                @error('role')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+
+
 
                         <div class="form-group row">
                             <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
